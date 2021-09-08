1f233f48a961fd39be085e2fd084569920b9a06b
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-08 00:02:02 -0500

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 100):
+        if ((i % 3) == 0) and ((i % 5) == 0):
+            print("FizzBuzz ", end="")
+        elif ((i % 3) == 0):
+            print("Fizz ", end="")
+        elif ((i % 5) == 0):
+            if (i != 100):
+                print("Buzz ", end="")
+            else:
+                print("Buzz", end="")
+        else:
+            print("{} ".format(i), end="")
