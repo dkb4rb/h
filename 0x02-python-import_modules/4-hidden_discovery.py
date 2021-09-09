5dfe7c342b4d6ba0cf440b8fe87f5ef584964508
DKBARB10 <84897255+DKBARB10@users.noreply.github.com>
2021-09-09 02:18:42 -0500

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+import marshal
 
-    import hidden_4
+s = open('hidden_4.pyc', 'rb')
+s.seek(8)
+code_obj = marshal.load(s)
 
-    for name in dir(hidden_4):
-        if name[:2] != '__':
-            print("{:s}".format(name))
+print(code_obj)
