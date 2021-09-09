931073e92df563065e3325e7f6fcc87cb8e5e2d5
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-08 20:16:11 -0500

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	"""Print the sum of the 1 and 2."""
-	from add_0 import add
-	a = 1
-	b = 2
-        print("{} + {} = {:d}".format(a, b, add(a, b))
+    """Print the sum of the 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {:d}".format(a, b, add(a, b))
