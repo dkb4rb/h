7e3d7e4a1ce2ada522a7e0e1609077edef4bad8c
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-28 02:13:35 -0500

    .

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # 0-safe_print_list.py
 
+
 def safe_print_list(my_list=[], x=0):
+
     sum = 0
     for i in range(x):
         try:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 2-safe_print_list_integers.py
 
+
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
     for i in range(0, x):
