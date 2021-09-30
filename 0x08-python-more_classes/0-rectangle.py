91a0fe4d9626572dc15b7ad7817a69e8df228151
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-29 21:47:00 -0500

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 0-rectangle.py
+# Juan Duque <3428@holbertonschool.com>
+
+
+class Rectangle:
+    def __init__(self) -> None:
+        """Define Class of the rectangle"""
+        pass
