08f899a0fa8d7643648c5c1f4f3723c7a3f4037f
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-09 12:10:16 -0500

    .

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,15 +11,15 @@ def matrix_divided(matrix, div):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
     size = None
-    for l in matrix:
-        if type(l) is not list:
+    for l1 in matrix:
+        if type(l1) is not list:
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(l1)
+        elif size != len(l1):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
+        for i in l1:
             if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
@@ -27,4 +27,4 @@ integers/floats")
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    return [[round(i / div, 2) for i in l1] for l1 in matrix]
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+from typing import Type
+
+
 def print_square(size):
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    
+    if size < 0:
+        raise TypeError("size must be >= 0")
     for i in range(size):
         [print("#", end="") for j in range(size)]
         print("")
