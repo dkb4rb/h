603c6a240211c68c60ff834066505f83c682b45a
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-09 16:21:38 -0500

    .

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,20 @@
 # Juan Duque <3428@holbertonschool.com>
 
 def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError("Text must be a string")
-    
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
