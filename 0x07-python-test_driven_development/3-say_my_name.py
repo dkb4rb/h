3279a1b0ffef7fe1cd478266f46b01f4bd8dbe52
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-09 17:22:06 -0500

    .

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,7 @@ This is the "3-say_my-name" module.
 The 3-say_my_name  module supplies one function, say_my_name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """Prints "My name is" followed by the first name and optional last name"""
     if type(first_name) is not str:
