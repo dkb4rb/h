647a615da4e5830030d98102d1726689b25c1ca1
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-09 12:16:56 -0500

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,6 +4,13 @@ from typing import Type
 
 
 def print_square(size):
+     """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
