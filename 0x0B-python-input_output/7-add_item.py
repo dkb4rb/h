c481a726928ede3529c525282e5a73846500e617
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 17:25:46 -0500

    .

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,19 +4,17 @@
 """Add all arguments to a Python list and save them to a file."""
 from sys import argv
 
-if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__(
-        "6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    filename = "add_item.json"
+filename = "add_item.json"
 
-    try:
-        json_list = load_from_json_file(filename)
-    except:
-        json_list = []
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
 
-    for arg in argv[1:]:
-        json_list.append(arg)
+for arg in argv[1:]:
+    json_list.append(arg)
 
-    save_to_json_file(json_list, filename)
+save_to_json_file(json_list, filename)
