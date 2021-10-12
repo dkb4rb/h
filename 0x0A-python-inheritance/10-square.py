aff489b3197f85573bf47da59e607e11ee436b87
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 21:40:23 -0500

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -19,3 +19,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
