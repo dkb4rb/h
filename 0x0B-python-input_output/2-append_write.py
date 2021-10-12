e921e20d6cceb104408f647fa1eddc872ec23dac
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 12:21:26 -0500

    .

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 2-append_write.py
+# Juan Duque <3428@holbertonschool.com>
+""" Funtion what append new text in file"""
+
+
+def append_write(filename="", text=""):
+    """ Read file with function of the append new text 
+        Return - 29"""
+    with open(filename, 'a') as file:
+        file.write(text)
+    return (29)
