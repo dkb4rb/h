14e9c4a22158c2c8303cbf5860e0cfc36c11ac06
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 20:20:40 -0500

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Juan Duque <3428@holbertonschool.com>
+""" Define the new Base of the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Represent the new BaseGeometry"""
+
+    """Define new mehod of instance PUBLIC"""
+
+    def area(self):
+        """ Raise exception of the function or not active"""
+        raise Exception("area() is not implemented")
+
+    pass
