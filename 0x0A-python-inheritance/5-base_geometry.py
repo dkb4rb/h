fc9b699776dd2f1dd3e5afb4cd27859afcdf8d4d
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 20:14:28 -0500

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Juan Duque <3428@holbertonschool.com>
+""" Define new empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Represent the new Class of name BaseGeometry"""
+    pass
