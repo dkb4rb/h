f8b275668efb72e97c4446442ec71c0bff9706a1
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 21:24:09 -0500

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,3 +2,34 @@
 # 9-rectangle.py
 # Juan Duque <3428@holbertonschool.com>
 """ Define the new class"""
+"""
+Impor the libraries
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ This is representation of the Rectangle"""
+
+    """ Instance the new function init"""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
