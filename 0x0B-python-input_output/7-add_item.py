26856669b7c8c789e87a40f34572b5112e971c7f
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 17:24:55 -0500

    .

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 7-add_item.py
+# Juan Duque <3428@holbertonschool.com>
+"""Add all arguments to a Python list and save them to a file."""
+from sys import argv
+
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__(
+        "6-load_from_json_file").load_from_json_file
+
+    filename = "add_item.json"
+
+    try:
+        json_list = load_from_json_file(filename)
+    except:
+        json_list = []
+
+    for arg in argv[1:]:
+        json_list.append(arg)
+
+    save_to_json_file(json_list, filename)
