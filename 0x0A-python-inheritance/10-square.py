060d089bc72e02216b503479b68d94c3d8df417e
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 21:44:05 -0500

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # 10-square.py
 # Juan Duque <3428@holbertonschool.com>
-"""
-Import the modul what contain the new rectangle
-"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 """Defines a Rectangle subclass Square."""
 
