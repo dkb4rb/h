a3dc83b67b6376913ba21c1224d769355e97d6d9
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-11 23:23:10 -0500

    101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,8 @@
 
 
 def add_attribute(obj, att, value):
+    """ If not is one dict of the hasattar return else of te othewise
+    return setattr"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, att, value)
