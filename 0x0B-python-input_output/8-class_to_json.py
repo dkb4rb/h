a71238ead0d4ee47c20dd16023d62deb55b63129
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 19:42:48 -0500

    .

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 8-class_to_json.py
 # Juan Duque <3428@holbertonschool.com>
-
-import json
+""" Declare the new function return new dict to json"""
 
 
 def class_to_json(obj):
+    """ Return obj in dict mode"""
     return obj.__dict__
