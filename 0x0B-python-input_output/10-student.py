5c91a076e1b964f18c947fd34da922c6b8c8e880
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 20:11:26 -0500

    .

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 9-student.py
+# Juan Duque <3428@holbertonschool.com>
+""" Define  a class Student that defines a student by
+    first_name
+    last_name
+    age
+    """
+
+
+class Student:
+
+    """ Instantiation the news attributes publics"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """ Declaration the new public method"""
+
+    def to_json(self, attrs=None):
+        """ Return obj in dict mode"""
+        if isinstance(attrs, list) and all(type(index) == str for index in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
