d1761c38e7374a4da71cb0de685107dfbb607888
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-12 21:34:09 -0500

    .

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 12-pascal_triangle.py
+# Juan Duque <3428@holbertonschool.com>
+""" Define one new pascal triangle """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    limit = n - 1
+    triangle = [[1]]
+
+    for i in range(limit):
+        row = []
+        row.append(1)
+
+        if len(triangle[i]) > 1:
+            prev_row_len = len(triangle[i]) - 1
+            nxt = 1
+
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
+                nxt += 1
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
