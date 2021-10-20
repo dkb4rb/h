7c62ef37aff16233fd29aec95a369d1a4994c520
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-20 18:28:27 -0500

    News Testing methods

--- 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
@@ -5,6 +5,7 @@
 
     Unittest Modules:
     """
+
 import os
 import unittest
 from models.base import Base
@@ -16,6 +17,26 @@ class test_base(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
+
+    def test_args(self):
+        b1 = Base(id=10)
+        b2 = Base(id=100)
+        self.assertIsNotNone(b1.id, b2.id)
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
 
 
 if __name__ == "__main__":
