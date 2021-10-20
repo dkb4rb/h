45e37e52abc9c5492daa9c772da7a1851c6a35ef
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-19 23:58:16 -0500

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 # base.py
 # Juan Duque <3428@holbertonschool.com>
-"""Import the libraries
-    unittest"""
+"""Import the libraries"""
 import json
 import csv
 import turtle
 
 
 class Base:
-
+    """ This is the reference of the Base class"""
     __nb_objects = 0
+
+    """ Instance new Base 
+    
+        Args: 
+            Id (int): Identity of the new Base"""
 
     def __init__(self, id=None):
 
