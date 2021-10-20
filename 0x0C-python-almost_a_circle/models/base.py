620ef26c4e491e7cf4ca10b69924dbcf09e98eb2
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-20 00:35:45 -0500

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,9 +11,8 @@ class Base:
     """ This is the reference of the Base class"""
     __nb_objects = 0
 
-    """ Instance new Base 
-    
-        Args: 
+    """ Instance new Base
+        Args:
             Id (int): Identity of the new Base"""
 
     def __init__(self, id=None):
