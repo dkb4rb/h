8231fd69e18ac95eae85c991bf941465bbe7e840
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-20 18:35:51 -0500

    News Class define REctangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,14 +5,13 @@
 import json
 import csv
 import turtle
-
-Base = __import__('base').Base
+from models.base import Base
 
 """ Define new class rectangle inherits of the class
     Base"""
 
 
-class rectangle(Base):
+class Rectangle(Base):
     """ This is reference to rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -21,14 +20,15 @@ class rectangle(Base):
         self.x = x
         self.y = y
         self.id = id
+        super().__init__(id)
 
 
 if __name__ == "__main__":
 
     list = ["", ""]
 
-    rec = rectangle(20, 30, 40, 50, 2312)
-    rec2 = rectangle(10, 20, 30, 40, 23)
+    rec = Rectangle(20, 30, 40, 50, 2312)
+    rec2 = Rectangle(10, 20, 30, 40, 23)
 
     list[0] = rec
     list[1] = rec2
