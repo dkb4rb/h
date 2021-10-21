ae0dc49bfab842b38f7860cb5485332d04635ad3
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-20 19:44:57 -0500

    Fixing getters and setters and value in self

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,20 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ This is reference to rectangle class"""
+    """ This is reference to rectangle class
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
