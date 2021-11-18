97247a41b316101328759f7b91217fcecd850d09
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-11-18 15:38:44 -0500

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- Create New user in sql for all database permissions
+-- in Mysql
 CREATE USER
 	IF NOT EXISTS user_0d_1@localhost
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
-	TO user_0d_1@'localhost;
+	TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
