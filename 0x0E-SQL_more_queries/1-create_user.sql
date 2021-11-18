f78abbe640f012a397392f822db9698c5da38682
root <root@RAIDEN.cybercloud.local>
2021-11-18 15:31:05 -0500

    Create new User with all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create New user in sql for all database permissions
+CREATE USER
+	IF NOT EXISTS user_0d_1@localhost
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO user_0d_1@localhost
+	IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
