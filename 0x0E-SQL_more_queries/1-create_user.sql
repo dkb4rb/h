4deae287e9739fea415258e2247ad7112058370d
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-11-18 15:38:01 -0500

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
 -- Create New user in sql for all database permissions
 CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IF NOT EXISTS user_0d_1@localhost
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+	TO user_0d_1@'localhost;
 FLUSH PRIVILEGES;
