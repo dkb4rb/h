155fa093b7ecd188d1c6923ceb302c77b3a1723b
root <root@RAIDEN.cybercloud.local>
2021-11-19 00:17:20 -0500

    Rotate the all tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows from hbtn_0d_tvshows.
+-- ordered by descending rating.
+SELECT title, SUM(rate) AS rating
+	FROM tv_shows AS t
+	INNER JOIN tv_show_ratings AS r ON t.id = r.show_id
+GROUP BY title
+ORDER BY `rating` DESC;
