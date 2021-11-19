a3bb50771524d7d12b8c647e992b7f94d41886c2
root <root@RAIDEN.cybercloud.local>
2021-11-18 23:56:25 -0500

    Show numbers generates

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- shows all genres from BD and counts the shows linked
+SELECT tv_genres.name AS genre, COUNT(*) number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
