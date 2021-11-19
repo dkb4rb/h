3b666e834f951bae81d36af33024a57b4707b7c7
root <root@RAIDEN.cybercloud.local>
2021-11-18 23:49:32 -0500

    Ncreates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates new db and a new table
+CREATE DATABASE
+	IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE
+	IF NOT EXISTS hbtn_0d_usa.states(
+		id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(256) NOT NULL
+	);
