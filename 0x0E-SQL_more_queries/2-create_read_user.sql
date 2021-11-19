8c922d428845ee1b83ef3cc41318b719176a5c12
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-11-18 21:57:33 -0500

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,6 @@ CREATE DATABASE
 CREATE USER
 	IF NOT EXISTS user_0d_2@localhost
 	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT user_0d_2@localhost
+GRANT SELECT
 	ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
