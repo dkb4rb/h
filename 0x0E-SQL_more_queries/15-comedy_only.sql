f43302dd70dd7b1b55dd15aa0b628c12e4b3e1f0
root <root@RAIDEN.cybercloud.local>
2021-11-18 23:59:12 -0500

    All task mandatory second deaddline

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- shows all comedy tv series from the DB
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
