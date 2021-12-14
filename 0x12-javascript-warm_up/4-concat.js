21c93d82cea0ddda1ee9c3bae9255691c307e286
root <root@localhost.localdomain>
2021-12-13 19:55:18 -0500

    COncat the text in argv

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const concat = ' is '
+console.log(typeof process.argv[2] === 'undefined' ? process.argv[2] + concat + process.argv[2] : process.argv[2] + concat + process.argv[3]);
