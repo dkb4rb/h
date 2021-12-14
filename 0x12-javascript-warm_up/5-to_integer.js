7b0e2f551b185b26ba7524439bc07a5892fd5cdd
root <root@localhost.localdomain>
2021-12-13 20:43:54 -0500

    PArseint number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const converint = parseInt(argv[2]);
+
+if (converint) { console.log('My number: ' + converint); } else { console.log('Not a Number'); }
