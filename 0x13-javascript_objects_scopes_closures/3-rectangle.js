f9fdfd0bcc03caab6b73fdd2962b2ddf2cc01877
root <root@localhost.localdomain>
2021-12-14 15:15:19 -0500

    Create new instance rectangle and news methods

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,4 +3,8 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
+
+  print (w, h) {
+    for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); }
+  }
 };
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+	constructor (w, h) {
+		if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+		}
+	print (w, h) {
+		for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width));}
+		}
+	rotate (w, h) {
+		[this.width, this.height] = [this.height, this.width];
+		}
+	double () {
+		this.width *= 2;
+		this.height *= 2;
+		}
+};
