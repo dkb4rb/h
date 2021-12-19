c788bf26dd1bd2ec030ded22aa1628707bd106be
root <root@localhost.localdomain>
2021-12-19 18:43:25 -0500

    Query to argv [4]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     prompt = db.cursor()
-    prompt.execute(
-        "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    prompt.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
     [print(state) for state in prompt.fetchall()]
     prompt.close()
     db.close()
