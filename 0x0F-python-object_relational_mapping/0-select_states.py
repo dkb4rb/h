5a4fbce334b4febdbad8625dd693bffd8ecb44e7
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-12-20 02:52:30 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,27 @@
 
 import sys
 import MySQLdb
+if __name__ == "__main__":
+    """ Values for the connect to the database """
+    users = sys.argv[1]
+    passw = sys.argv[2]
+    datB = sys.argv[3]
+    """Connecting to the database"""
+    db = MySQLdb.connect(host="localhost", user=users,
+                         password=passw, db=datB, port=3306, charset="utf8")
 
-if name == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    prompt = db.cursor()
-    prompt.execute("SELECT * FROM states")
-    [print[state] for state in prompt.fetchall()]
+    """The cursor gives us the ability to have multiple seperate
+    working environments through the same connection to the database"""
+    cur = db.cursor()
+
+    """Executing the query"""
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    states = cur.fetchall()
+
+    """ Show the results find """
+    for state in states:
+        print(state)
+    cur.close()
+    """Closing the connection to the database"""
+    db.close()
